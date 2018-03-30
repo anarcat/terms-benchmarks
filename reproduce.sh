bb01a0527346e905ee330916c388728ddd3a2ca7
Antoine Beaupre <anarcat@debian.org>
2018-03-30 12:45:37 -0400

    try to disable screen idle, failing: it does not lock but still idles out

--- reproduce.sh
+++ reproduce.sh
@@ -13,6 +13,14 @@ test="$PWD/bw-test.sh $lines"
 echo "setting Xresources to defaults"
 xrdb -load /dev/null
 
+echo "disabling lock screen"
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver lock-delay 86400
+
+echo "starting test in 3 seconds, switch to a blank workspace"
+sleep 3
+
 echo "terminal,time,cpu,memory" >> times-${samples}x${lines}.csv
 
 for terminal in konsole pterm terminator uxterm xfce4-terminal  ; do
@@ -33,6 +41,3 @@ for terminal in alacritty mlterm stterm urxvt; do
     done
     sed -n "/elapsed/{s/^.* 0:\([0-9]*.[0-9]*\)elapsed \([0-9?]*\)%CPU .*avgdata \([0-9]*\)maxresident.*\$/$terminal,\1,\2,\3/;p}" < $terminal-time.txt >> times-${samples}x${lines}.csv
 done
-
-echo "resetting Xresources"
-xrdb -load ~/.Xresources
